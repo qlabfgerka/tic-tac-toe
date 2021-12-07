45e3eb14808ad06ba5b47c7ec24f6ea58e7bafd2
Jan Premzl <71296359+qlabfgerka@users.noreply.github.com>
2021-12-07 17:09:29 +0100

    Project initialization (#1)
    - created angular frontend project
    - created spring backend project

---
+++ ttt-backend/src/main/java/com/example/tttbackend/TttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tttbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttBackendApplication.class, args);
+	}
+
+}
