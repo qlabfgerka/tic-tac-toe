fc57315fe891749b572df90c01b3b5af03bfc469
Jan Premzl <jan.premzl16@gmail.com>
2021-12-07 17:08:27 +0100

    Project initialization
    - created angular frontend project
    - created spring backend project

---
+++ ttt-backend/src/test/java/com/example/tttbackend/TttBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tttbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
